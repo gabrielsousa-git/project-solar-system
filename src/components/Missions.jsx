84e59957db7a0f8ee96d3b27a9d42ee9bab04df5
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-07-20 19:01:34 -0300

    Requisito 10 concluido

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
+    // eslint-disable-next-line max-len
+    const missionsHtmlElements = missions.map((mission) => <MissionCard key={ mission.name } name={ mission.name } year={ mission.year } country={ mission.country } destination={ mission.destination } />);
+
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missionsHtmlElements }
       </div>
     );
   }
